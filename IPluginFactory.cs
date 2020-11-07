a6700ab5f343a2c5dbe39b6d10fd4d4d4117dcb0
FilatovAV <a777f@mail.ru>
2020-11-07 23:48:58 +0300

    Add project files.

---
+++ IPluginFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ds.test.impl
+{
+    interface IPluginFactory
+    {
+        int PluginsCount { get; }
+        string[] GetPluginNames { get; }
+        IPlugin GetPlugin(string pluginName);
+    }
+}
---
+++ Sum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Resources;
+using System.Text;
+
+namespace ds.test.impl
+{
+    internal class Sum : IPlugin
+    {
+        internal Sum()
+        {
+            Image = Properties.Resources.Sum;
+        }
+        public string PluginName { get; } = "Сложение чисел";
+        public string Version { get; } = "1.0";
+        public Image Image { get; }
+        public string Description { get; private set; } = "Плагин выполняет операции сложения.";
+        public int Run(int input1, int input2) => input1 + input2;
+    }
+}
